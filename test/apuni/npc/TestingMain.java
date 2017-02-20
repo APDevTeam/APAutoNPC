5689539da39aba2476bbcdda82dd48510a4c67b1
WhiteWolfdoge <emjwhite@outlook.com>
2017-02-19 21:25:43 -0800

    Testing source folder and method

---
+++ test/apuni/npc/TestingMain.java
@@ -0,0 +1,12 @@
+package apuni.npc;
+
+public class TestingMain{
+	
+	public static void main(String[] args){
+		double bal = 3832426.348;
+		
+		String test = Main.formatBalance(Main.MERCHANT_BUILDING_BLOCKS_PRICE);
+		System.out.println(test);
+	}
+	
+}
